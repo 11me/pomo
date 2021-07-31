cfc7a51b29591e4d2cf69a3ea54bb6e128a12880
lime <osmium760@gmail.com>
2021-07-31 15:11:58 +0300

    new feature: user notification

--- pomod/lib/pomo_struct.go
+++ pomod/lib/pomo_struct.go
@@ -2,16 +2,18 @@ package lib
 
 import (
 	"fmt"
+	"os/exec"
 	"time"
 )
 
 // Pomo struct represents a pomodoro.
 type Pomo struct {
-	Status string // status running or not could be ON or OFF
-	Time   time.Duration
-	Emoji  string    // default tomato emoji
-	Blink  [2]string // array of 2 emoji to create an animation
-	Cancel chan bool // channel for cancelation a running timer goroutine
+	Status   string // status running or not could be ON or OFF
+	Time     time.Duration
+	Notified bool
+	Emoji    string    // default tomato emoji
+	Blink    [2]string // array of 2 emoji to create an animation
+	Cancel   chan bool // channel for cancelation a running timer goroutine
 }
 
 // Constants to represent a status of the pomo.
@@ -20,6 +22,8 @@ const (
 	OFF = "off"
 )
 
+const NOTIFICATION_MESSAGE = "🍅 Time to take a break!"
+
 var DEFAULT_DURATION, _ = time.ParseDuration("25m")
 
 // Start starts the pomodoto timer by running a goroutine.
@@ -27,7 +31,9 @@ func (pomo *Pomo) Start() {
 
 	// Start the goroutine if not already running.
 	if pomo.Status == OFF {
+
 		pomo.Status = ON
+		pomo.Notified = false
 
 		// goroutine cancelation channel
 		cancel := make(chan bool)
@@ -67,6 +73,9 @@ func (pomo *Pomo) Stop() {
 // it accepts time.Duration parsable format.
 func (pomo *Pomo) SetDuration(d time.Duration) {
 
+	// change notified status when duration is changed
+	pomo.Notified = false
+
 	// if pomo is running
 	if pomo.Status == ON {
 
@@ -89,8 +98,17 @@ func (pomo *Pomo) GetDuration() string {
 	}
 
 	// if pomo run out of time that was set
-	// make a blinking animation
+	// make a blinking animation and send ntification
 	if pomo.Time < 0 {
+
+		// if user not notified
+		if !pomo.Notified {
+
+			// notify the user
+			go notifyUser(NOTIFICATION_MESSAGE)
+
+			pomo.Notified = true
+		}
 
 		// emoji_id is a number between 0 and 1
 		emoji_id := (pomo.Time.Milliseconds() / 1000 % 2) * (-1)
@@ -104,4 +122,12 @@ func (pomo *Pomo) GetDuration() string {
 // Restart restarts the pomo timer to default value.
 func (pomo *Pomo) Restart() {
 	pomo.SetDuration(DEFAULT_DURATION)
+}
+
+// NotifyUser uses external notify-send command
+// sends specified message.
+func notifyUser(msg string) {
+	cmd := exec.Command("notify-send", "-u", "critical", msg)
+	cmd.Run()
+	return
 }
